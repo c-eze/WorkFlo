73e1b90d24b6ffdff09bc1face7ae0c4175a41f8
Chikere <chikere.o.ezekannagha@outlook.com>
2025-02-15 17:46:07 -0600

    add invite controller and actions

---
+++ Controllers/InvitesController.cs
@@ -0,0 +1,299 @@
+using AspnetCoreMvcFull.Data;
+using AspnetCoreMvcFull.Extensions;
+using AspnetCoreMvcFull.Models;
+using AspnetCoreMvcFull.Security;
+using AspnetCoreMvcFull.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspnetCoreMvcFull.Controllers;
+
+public class InvitesController : Controller
+{
+  private readonly ApplicationDbContext _context;
+  private readonly IBTInviteService _inviteService;
+  private readonly IBTProjectService _projectService;
+  private readonly IEmailSender _emailService;
+  private readonly IDataProtector _protector;
+
+  public InvitesController(ApplicationDbContext context,
+               IDataProtectionProvider dataProtectionProvider,
+               DataProtectionPurposeStrings dataProtectionPurposeStrings,
+               IBTInviteService inviteService,
+               IEmailSender emailService,
+               IBTProjectService projectService)
+  {
+    _context = context;
+    _protector = dataProtectionProvider.CreateProtector(dataProtectionPurposeStrings.InviteRouteValue);
+    _inviteService = inviteService;
+    _emailService = emailService;
+    _projectService = projectService;
+  }
+
+  // GET: Invites
+  public async Task<IActionResult> Index()
+  {
+    int companyId = User.Identity.GetCompanyId().Value;
+
+    List<Invite> invites = await _inviteService.GetAllInvitesByCompanyAsync(companyId);
+
+    return View(invites);
+  }
+
+  public async Task<IActionResult> Resend(int id)
+  {
+    int companyId = User.Identity.GetCompanyId().Value;
+
+    Invite invite = await _inviteService.GetInviteAsync(id, companyId);
+
+    ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Name", invite.CompanyId);
+    ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", invite.ProjectId);
+
+    return View(invite);
+  }
+
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Resend([Bind("Id,InviteDate,InviteDate,CompanyId,ProjectId,InviterId,InviteeEmail,InviteeFirstName,InviteeLastName,IsValid")] Invite invite)
+  {
+    if (ModelState.IsValid)
+    {
+      invite.EncryptedToken = _protector.Protect(invite.CompanyToken.ToString());
+      invite.EncryptedEmail = _protector.Protect(invite.InviteeEmail);
+      invite.EncryptedCompanyId = _protector.Protect(invite.CompanyId.ToString());
+
+      var returnUrl = Url.Action(
+        "ProcessInvite",
+        "Invites",
+        new { token = invite.EncryptedToken, email = invite.EncryptedEmail, companyId = invite.EncryptedCompanyId },
+        protocol: Request.Scheme);
+
+      await _inviteService.UpdateInviteAsync(invite);
+      await _emailService.SendEmailAsync(invite.InviteeEmail, $"Workflo Invitation", $"Hello {invite.InviteeFirstName} {invite.InviteeLastName},<br><br>Click the following link to join our team on Workflo: <a href='{returnUrl}'>Click Here</a>.<br><br>If clicking the link above does not take you to the activation screen, paste the following link into your browser's URL:<br>{returnUrl}<br><br>Thank you,<br>Admin");
+      return RedirectToAction("Index", "Invites");
+    }
+    ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Name", invite.CompanyId);
+    ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", invite.ProjectId);
+    return View(invite);
+  }
+
+  public async Task<IActionResult> ProcessInvite(string token, string email, string companyId)
+  {
+    var inviteToken = Guid.Parse(_protector.Unprotect(token));
+    var inviteMail = _protector.Unprotect(email);
+    var inviteCompanyId = Convert.ToInt32(_protector.Unprotect(companyId));
+
+    var validationResult = await _inviteService.ValidateInviteCodeAsync(inviteToken);
+    if (validationResult)
+    {
+      return RedirectToPage("/Account/RegisterByInvite", new { area = "Identity", token = inviteToken, email = inviteMail, companyId = inviteCompanyId });
+    }
+
+    return View("/Account/Login", new { area = "Identity" });
+  }
+
+  // GET: Invites/Archive/5
+  [Authorize(Roles = "Admin")]
+  public async Task<IActionResult> Archive(int? id)
+  {
+    if (id == null)
+    {
+      return NotFound();
+    }
+
+    int companyId = User.Identity.GetCompanyId().Value;
+
+    Invite invite = await _inviteService.GetInviteAsync(id.Value, companyId);
+
+    if (invite == null)
+    {
+      return NotFound();
+    }
+
+    return View(invite);
+  }
+
+  // POST: Invites/Archive/5
+  [Authorize(Roles = "Admin")]
+  [HttpPost, ActionName("Archive")]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> ArchiveConfirmed(int id)
+  {
+    int companyId = User.Identity.GetCompanyId().Value;
+
+    Invite invite = await _inviteService.GetInviteAsync(id, companyId);
+    invite.Archived = true;
+    await _inviteService.UpdateInviteAsync(invite);
+
+    return RedirectToAction(nameof(Index));
+  }
+
+  // GET: Invites/Details/5
+  public async Task<IActionResult> Details(int? id)
+  {
+    if (id == null || _context.Invites == null)
+    {
+      return NotFound();
+    }
+
+    var invite = await _context.Invites
+        .Include(i => i.Company)
+        .Include(i => i.Invitee)
+        .Include(i => i.Project)
+        .FirstOrDefaultAsync(m => m.Id == id);
+    if (invite == null)
+    {
+      return NotFound();
+    }
+
+    return View(invite);
+  }
+
+  // GET: Invites/Create
+  public async Task<IActionResult> Create()
+  {
+    int companyId = User.Identity.GetCompanyId().Value;
+    var projects = await _projectService.GetAllProjectsByCompanyAsync(companyId);
+
+    ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Name", companyId);
+    ViewData["ProjectId"] = new SelectList(projects, "Id", "Name");
+
+    return View();
+  }
+
+  // POST: Invites/Create
+  // To protect from overposting attacks, enable the specific properties you want to bind to.
+  // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Create([Bind("InviteDate,CompanyId,ProjectId,InviterId,InviteeEmail,InviteeFirstName,InviteeLastName,IsValid")] Invite invite)
+  {
+    if (ModelState.IsValid)
+    {
+      invite.CompanyToken = Guid.NewGuid();
+      invite.EncryptedToken = _protector.Protect(invite.CompanyToken.ToString());
+      invite.EncryptedEmail = _protector.Protect(invite.InviteeEmail);
+      invite.EncryptedCompanyId = _protector.Protect(invite.CompanyId.ToString());
+
+      var returnUrl = Url.Action(
+          "ProcessInvite",
+          "Invites",
+          new { token = invite.EncryptedToken, email = invite.EncryptedEmail, companyId = invite.EncryptedCompanyId },
+          protocol: Request.Scheme);
+
+      await _inviteService.AddNewInviteAsync(invite);
+      await _emailService.SendEmailAsync(invite.InviteeEmail, $"Workflo Invitation", $"Hello {invite.InviteeFirstName} {invite.InviteeLastName},<br><br>Click the following link to join our team on Workflo: <a href='{returnUrl}'>Click Here</a>.<br><br>If clicking the link above does not take you to the activation screen, paste the following link into your browser's URL:<br>{returnUrl}<br><br>Thank you,<br>Admin");
+      return RedirectToAction("Index", "Invites");
+    }
+    ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Name", invite.CompanyId);
+    ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", invite.ProjectId);
+    return View(invite);
+  }
+
+  // GET: Invites/Edit/5
+  public async Task<IActionResult> Edit(int? id)
+  {
+    if (id == null || _context.Invites == null)
+    {
+      return NotFound();
+    }
+
+    var invite = await _context.Invites.FindAsync(id);
+    if (invite == null)
+    {
+      return NotFound();
+    }
+    ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Id", invite.CompanyId);
+    ViewData["InviteeId"] = new SelectList(_context.Users, "Id", "Id", invite.InviteeId);
+    ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", invite.ProjectId);
+    return View(invite);
+  }
+
+  // POST: Invites/Edit/5
+  // To protect from overposting attacks, enable the specific properties you want to bind to.
+  // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+  [HttpPost]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> Edit(int id, [Bind("Id,InviteDate,JoinDate,CompanyToken,CompanyId,ProjectId,InviterId,InviteeId,InviteeEmail,InviteeFirstName,InviteeLastName,IsValid")] Invite invite)
+  {
+    if (id != invite.Id)
+    {
+      return NotFound();
+    }
+
+    if (ModelState.IsValid)
+    {
+      try
+      {
+        _context.Update(invite);
+        await _context.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!InviteExists(invite.Id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return RedirectToAction(nameof(Index));
+    }
+    ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Id", invite.CompanyId);
+    ViewData["InviteeId"] = new SelectList(_context.Users, "Id", "Id", invite.InviteeId);
+    ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", invite.ProjectId);
+    return View(invite);
+  }
+
+  // GET: Invites/Delete/5
+  public async Task<IActionResult> Delete(int? id)
+  {
+    if (id == null || _context.Invites == null)
+    {
+      return NotFound();
+    }
+
+    var invite = await _context.Invites
+        .Include(i => i.Company)
+        .Include(i => i.Invitee)
+        .Include(i => i.Project)
+        .FirstOrDefaultAsync(m => m.Id == id);
+    if (invite == null)
+    {
+      return NotFound();
+    }
+
+    return View(invite);
+  }
+
+  // POST: Invites/Delete/5
+  [HttpPost, ActionName("Delete")]
+  [ValidateAntiForgeryToken]
+  public async Task<IActionResult> DeleteConfirmed(int id)
+  {
+    if (_context.Invites == null)
+    {
+      return Problem("Entity set 'ApplicationDbContext.Invites'  is null.");
+    }
+    var invite = await _context.Invites.FindAsync(id);
+    if (invite != null)
+    {
+      _context.Invites.Remove(invite);
+    }
+
+    await _context.SaveChangesAsync();
+    return RedirectToAction(nameof(Index));
+  }
+
+  private bool InviteExists(int id)
+  {
+    return (_context.Invites?.Any(e => e.Id == id)).GetValueOrDefault();
+  }
+}
+
--- Services/BTInviteService.cs
+++ Services/BTInviteService.cs
@@ -3,208 +3,207 @@ using AspnetCoreMvcFull.Models;
 using AspnetCoreMvcFull.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-namespace AspnetCoreMvcFull.Services
+namespace AspnetCoreMvcFull.Services;
+
+public class BTInviteService : IBTInviteService
 {
-  public class BTInviteService : IBTInviteService
+  #region Properties
+  private readonly ApplicationDbContext _context;
+  #endregion
+
+  #region Constructor
+  public BTInviteService(ApplicationDbContext context)
   {
-    #region Properties
-    private readonly ApplicationDbContext _context;
-    #endregion
+    _context = context;
+  }
+  #endregion
 
-    #region Constructor
-    public BTInviteService(ApplicationDbContext context)
+  #region Accept Invite
+  public async Task<bool> AcceptInviteAsync(Guid token, string userId)
+  {
+    Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+
+    if (invite == null)
     {
-      _context = context;
+      return false;
     }
-    #endregion
 
-    #region Accept Invite
-    public async Task<bool> AcceptInviteAsync(Guid token, string userId)
+    try
     {
-      Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+      invite.IsValid = false;
+      invite.InviteeId = userId;
+      invite.JoinDate = DateTimeOffset.Now;
+      await _context.SaveChangesAsync();
 
-      if (invite == null)
-      {
-        return false;
-      }
-
-      try
-      {
-        invite.IsValid = false;
-        invite.InviteeId = userId;
-        invite.JoinDate = DateTimeOffset.Now;
-        await _context.SaveChangesAsync();
-
-        return true;
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
+      return true;
     }
-    #endregion
-
-    #region Add New Invite
-    public async Task AddNewInviteAsync(Invite invite)
+    catch (Exception)
     {
-      try
-      {
-        await _context.Invites.AddAsync(invite);
-        await _context.SaveChangesAsync();
-      }
-      catch (Exception)
-      {
 
-        throw;
-      }
+      throw;
     }
-    #endregion
+  }
+  #endregion
 
-    #region Any Invite
-    public async Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+  #region Add New Invite
+  public async Task AddNewInviteAsync(Invite invite)
+  {
+    try
     {
-      try
-      {
-        bool result = await _context.Invites
-            .Where(i => i.CompanyId == companyId)
-            .AnyAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
-
-        return result;
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
+      await _context.Invites.AddAsync(invite);
+      await _context.SaveChangesAsync();
     }
-    #endregion
-
-    #region Archive Invite
-    public async Task ArchiveInviteAsync(Invite invite)
+    catch (Exception)
     {
-      try
-      {
-        invite.Archived = true;
-        _context.Update(invite);
-        await _context.SaveChangesAsync();
-      }
-      catch (Exception)
-      {
 
-        throw;
-      }
+      throw;
     }
-    #endregion
+  }
+  #endregion
 
-    #region Get All Invites By Company
-    public async Task<List<Invite>> GetAllInvitesByCompanyAsync(int companyId)
+  #region Any Invite
+  public async Task<bool> AnyInviteAsync(Guid token, string email, int companyId)
+  {
+    try
     {
-      try
-      {
-        List<Invite> invites = await _context.Invites
+      bool result = await _context.Invites
+          .Where(i => i.CompanyId == companyId)
+          .AnyAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+
+      return result;
+    }
+    catch (Exception)
+    {
+
+      throw;
+    }
+  }
+  #endregion
+
+  #region Archive Invite
+  public async Task ArchiveInviteAsync(Invite invite)
+  {
+    try
+    {
+      invite.Archived = true;
+      _context.Update(invite);
+      await _context.SaveChangesAsync();
+    }
+    catch (Exception)
+    {
+
+      throw;
+    }
+  }
+  #endregion
+
+  #region Get All Invites By Company
+  public async Task<List<Invite>> GetAllInvitesByCompanyAsync(int companyId)
+  {
+    try
+    {
+      List<Invite> invites = await _context.Invites
+        .Where(i => i.CompanyId == companyId)
+        .Include(i => i.Company)
+        .Include(i => i.Invitor)
+        .Include(i => i.Project)
+        .ToListAsync();
+      return invites;
+    }
+    catch (Exception)
+    {
+      throw;
+    }
+  }
+  #endregion
+
+  #region Get Invite
+  public async Task<Invite> GetInviteAsync(int inviteId, int companyId)
+  {
+    try
+    {
+      Invite invite = await _context.Invites
           .Where(i => i.CompanyId == companyId)
           .Include(i => i.Company)
           .Include(i => i.Invitor)
           .Include(i => i.Project)
-          .ToListAsync();
-        return invites;
-      }
-      catch (Exception)
-      {
-        throw;
-      }
-    }
-    #endregion
+          .FirstOrDefaultAsync(i => i.Id == inviteId);
 
-    #region Get Invite
-    public async Task<Invite> GetInviteAsync(int inviteId, int companyId)
+      return invite;
+    }
+    catch (Exception)
     {
-      try
-      {
-        Invite invite = await _context.Invites
-            .Where(i => i.CompanyId == companyId)
-            .Include(i => i.Company)
-            .Include(i => i.Invitor)
-            .Include(i => i.Project)
-            .FirstOrDefaultAsync(i => i.Id == inviteId);
 
-        return invite;
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
+      throw;
     }
-    #endregion
-
-    #region Get Invite
-    public async Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
-    {
-      try
-      {
-        Invite invite = await _context.Invites
-      .Where(i => i.CompanyId == companyId)
-      .Include(i => i.Company)
-      .Include(i => i.Invitor)
-      .Include(i => i.Project)
-      .FirstOrDefaultAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
-
-        return invite;
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
-    }
-    #endregion
-
-    #region Update Invite
-    public async Task UpdateInviteAsync(Invite invite)
-    {
-      try
-      {
-        _context.Update(invite);
-        await _context.SaveChangesAsync();
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
-    }
-    #endregion
-
-    #region Validate Invite Code
-    public async Task<bool> ValidateInviteCodeAsync(Guid token)
-    {
-      if (token == null)
-      {
-        return false;
-      }
-
-      bool result = false;
-
-      Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
-
-      if (invite != null)
-      {
-        //determine invite date
-        DateTime inviteDate = invite.InviteDate.DateTime;
-
-        //custom validation of invite based on the date it was issued
-        //in this case we are allowing an invite to be valid for 7 days
-        bool validDate = (DateTime.Now - inviteDate).TotalDays <= 7;
-
-        if (validDate)
-        {
-          result = invite.IsValid;
-        }
-      }
-      return result;
-    }
-    #endregion
   }
+  #endregion
+
+  #region Get Invite
+  public async Task<Invite> GetInviteAsync(Guid token, string email, int companyId)
+  {
+    try
+    {
+      Invite invite = await _context.Invites
+    .Where(i => i.CompanyId == companyId)
+    .Include(i => i.Company)
+    .Include(i => i.Invitor)
+    .Include(i => i.Project)
+    .FirstOrDefaultAsync(i => i.CompanyToken == token && i.InviteeEmail == email);
+
+      return invite;
+    }
+    catch (Exception)
+    {
+
+      throw;
+    }
+  }
+  #endregion
+
+  #region Update Invite
+  public async Task UpdateInviteAsync(Invite invite)
+  {
+    try
+    {
+      _context.Update(invite);
+      await _context.SaveChangesAsync();
+    }
+    catch (Exception)
+    {
+
+      throw;
+    }
+  }
+  #endregion
+
+  #region Validate Invite Code
+  public async Task<bool> ValidateInviteCodeAsync(Guid token)
+  {
+    if (token == null)
+    {
+      return false;
+    }
+
+    bool result = false;
+
+    Invite invite = await _context.Invites.FirstOrDefaultAsync(i => i.CompanyToken == token);
+
+    if (invite != null)
+    {
+      //determine invite date
+      DateTime inviteDate = invite.InviteDate.DateTime;
+
+      //custom validation of invite based on the date it was issued
+      //in this case we are allowing an invite to be valid for 7 days
+      bool validDate = (DateTime.Now - inviteDate).TotalDays <= 7;
+
+      if (validDate)
+      {
+        result = invite.IsValid;
+      }
+    }
+    return result;
+  }
+  #endregion
 }
