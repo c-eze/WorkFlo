b9bff9c05bffba382ec192e5e932a1a79ee93e4d
Chikere <chikere.o.ezekannagha@outlook.com>
2025-09-14 15:27:41 -0500

    update email service

--- Services/Interfaces/BTEmailService.cs
+++ Services/Interfaces/BTEmailService.cs
@@ -2,8 +2,11 @@ using AspnetCoreMvcFull.Models;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
 using Microsoft.Extensions.Options;
 using MimeKit;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace AspnetCoreMvcFull.Services.Interfaces
 {
@@ -23,40 +26,59 @@ namespace AspnetCoreMvcFull.Services.Interfaces
     #region Send Email
     public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
     {
-      var emailSender = _mailSettings.Email ?? Environment.GetEnvironmentVariable("Email");
-
-      MimeMessage email = new();
-
-      email.Sender = MailboxAddress.Parse(emailSender);
-      email.To.Add(MailboxAddress.Parse(emailTo));
-      email.Subject = subject;
-
-      var builder = new BodyBuilder
+      var options = new RestClientOptions("https://api.mailgun.net")
       {
-        HtmlBody = htmlMessage
+        Authenticator = new HttpBasicAuthenticator("api", Environment.GetEnvironmentVariable("MailPassword") ?? _mailSettings.MailPassword)
       };
 
-      email.Body = builder.ToMessageBody();
-
-      using var smtp = new SmtpClient();
-      try
-      {
-        var host = _mailSettings.MailHost ?? Environment.GetEnvironmentVariable("MailHost");
-        var port = _mailSettings.MailPort != 0 ? _mailSettings.MailPort : int.Parse(Environment.GetEnvironmentVariable("MailPort")!);
-        var password = _mailSettings.MailPassword ?? Environment.GetEnvironmentVariable("MailPassword");
-
-        await smtp.ConnectAsync(host, port, SecureSocketOptions.StartTls);
-        await smtp.AuthenticateAsync(emailSender, password);
-
-        await smtp.SendAsync(email);
-        smtp.Disconnect(true);
-      }
-      catch (Exception)
-      {
-
-        throw;
-      }
+      var client = new RestClient(options);
+      var request = new RestRequest("/v3/sandbox0b6ecaaca57746eead36336b93db5677.mailgun.org/messages", RestSharp.Method.Post);
+      request.AlwaysMultipartFormData = true;
+      request.AddParameter("from", "Workflo <hi@chikere.dev>");
+      request.AddParameter("to", emailTo);
+      request.AddParameter("subject", subject); 
+      request.AddParameter("text", htmlMessage);
+      await client.ExecuteAsync(request);  
     }
     #endregion
+
+    //#region Send Email
+    //public async Task SendEmailAsync(string emailTo, string subject, string htmlMessage)
+    //{
+    //  var emailSender = _mailSettings.Email ?? Environment.GetEnvironmentVariable("Email");
+
+    //  MimeMessage email = new();
+
+    //  email.Sender = MailboxAddress.Parse(emailSender);
+    //  email.To.Add(MailboxAddress.Parse(emailTo));
+    //  email.Subject = subject;
+
+    //  var builder = new BodyBuilder
+    //  {
+    //    HtmlBody = htmlMessage
+    //  };
+
+    //  email.Body = builder.ToMessageBody();
+
+    //  using var smtp = new SmtpClient();
+    //  try
+    //  {
+    //    var host = _mailSettings.MailHost ?? Environment.GetEnvironmentVariable("MailHost");
+    //    var port = _mailSettings.MailPort != 0 ? _mailSettings.MailPort : int.Parse(Environment.GetEnvironmentVariable("MailPort")!);
+    //    var password = _mailSettings.MailPassword ?? Environment.GetEnvironmentVariable("MailPassword");
+
+    //    await smtp.ConnectAsync(host, port, SecureSocketOptions.StartTls);
+    //    await smtp.AuthenticateAsync(emailSender, password);
+
+    //    await smtp.SendAsync(email);
+    //    smtp.Disconnect(true);
+    //  }
+    //  catch (Exception)
+    //  {
+
+    //    throw;
+    //  }
+    //}
+    //#endregion
   }
 }
