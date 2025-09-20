87231d092d29850cb1b577bea13dfb4f0e9166ce
Chikere <chikere.o.ezekannagha@outlook.com>
2025-09-20 14:23:02 -0500

    update email service

--- Services/Interfaces/BTEmailService.cs
+++ Services/Interfaces/BTEmailService.cs
@@ -32,13 +32,26 @@ namespace AspnetCoreMvcFull.Services.Interfaces
 
       var emailSender = _mailSettings.Email ?? Environment.GetEnvironmentVariable("Email");
 
+      //MimeMessage email = new();
+
+      //email.Sender = MailboxAddress.Parse(emailSender);
+      //email.To.Add(MailboxAddress.Parse(emailTo));
+      //email.Subject = subject;
+
+      var builder = new BodyBuilder
+      {
+        HtmlBody = htmlMessage
+      };
+
+      //email.Body = builder.ToMessageBody();
+
       var client = new RestClient(options);
       var request = new RestRequest("/v3/chikere.dev/messages.mime", Method.Post);
       request.AlwaysMultipartFormData = true;
       request.AddParameter("from", $"Workflo <{emailSender}>");
       request.AddParameter("to", $"<{emailTo}>");
       request.AddParameter("subject", subject); 
-      request.AddParameter("text", htmlMessage);
+      request.AddParameter("text", builder.ToString());
       await client.ExecuteAsync(request);  
     }
     #endregion
